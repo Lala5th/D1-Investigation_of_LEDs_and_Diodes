2f11edc3ec20960e82132f864b1a5abcb27604f3
Lala5th <lala5th@gmail.com>
2020-11-09 20:31:15 +0000

    Added DAQ and updated readme

---
+++ DAQ.py
@@ -0,0 +1,40 @@
+import pyvisa
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from time import sleep
+
+plt.ion()
+args = sys.argv
+rm = pyvisa.ResourceManager()
+inst = rm.open_resource('USB0::0x05E6::0x2450::04392009::INSTR')
+
+inst.write('smu.measure.func = smu.FUNC_DC_VOLTAGE')
+inst.write('smu.source.func  = smu.FUNC_DC_CURRENT')
+inst.write('smu.source.vlimit.level = 5')
+inst.write("smu.source.output = smu.ON")
+
+I0 = float(args[1])
+Im = float(args[2])
+Istep = float(args[3])
+
+if Im >= 0.3 or I0 >= 0.3:
+	sys.exit()
+
+Is = np.arange(I0,Im,Istep)
+data = []
+inst.write("defbuffer1.clear()")
+for I in Is:
+	inst.write("smu.source.level = " + str(I))
+	#sleep(0.001)
+	d = inst.query("print(smu.measure.read(defbuffer1),defbuffer1.sourcevalues[1])")
+	print(d)
+	inst.write("defbuffer1.clear()")
+	ds = d.split('\t')
+	data.append([float(ds[0]),float(ds[1])])
+
+save_data = np.array(data)
+plt.plot(save_data[:,0],save_data[:,1])
+
+fname = args[4]
+np.savetxt(fname,save_data,delimiter=',')
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Description
 This is the data and code for analysis taken for Imperials third year experiment D1-Investigation of LEDs and diodes.
+
+# Dataset naming convention
+
+Each dataset has a specific name combined of several parts. The general format is:
+setup-Temperature[_DAQ].csv,
+
+`setup` is the setup used, which is made up of 2 parts, a number and a letter. 's' is the fist setup that was used and references the specific cable and resistor, while now it has been replaced mostly with setup 'b' due to its modularity. Setup b has total resistance of ~17 Ohms. Below is a list of some setup pairings, indicating the diodes contained:
+- s1: Diode is 1N4001
+- b1: Diode is 1N5401
+- b2: Diode is 1N4148
+- b3: Diode is 1N4001
+
+Any datasets after this will bear the ID of the diode on them. The temperature is either RT (room temperature) or LN (Liquid nitrogen).
